7af68df056dafb93e9f02d00ce438dea321ee87e
jhondev-code <jhondev.code@gmail.com>
2021-03-31 09:39:12 -0500

    chapter 2 - data types

---
+++ chapter02/exercises/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("32.132 * 42.452 =", 32.132 * 42.452)
+    fmt.Println("(true && false) || (false && true) || !(false && false) =", (true && false) || (false && true) || !(false && false))
+}
